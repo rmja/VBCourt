407d2b644335bc4ddc678a33a56daace2d7cdc2d
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-10-21 13:29:55 +0200

    Add team password

--- src/VBCourt.Api/Models/Team.cs
+++ src/VBCourt.Api/Models/Team.cs
@@ -8,6 +8,9 @@ namespace VBCourt.Api.Models
         [MaxLength(100)]
         public string Name { get; set; }
         public int Number { get; init; }
+        [MaxLength(20)]
+        public string? Password { get; set; }
+        public bool PasswordRequired => Password is not null;
 
         public Team(string name)
         {
